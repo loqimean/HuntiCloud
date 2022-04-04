6d8d7acdb6a69015005c36ce7169862e34728b8e
loqimean <vanuha277@gmail.com>
2022-04-04 08:35:36 +0300

    chore(js) toggle switcher by load

--- app/javascript/controllers/dark_theme_controller.js
+++ app/javascript/controllers/dark_theme_controller.js
@@ -1,6 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ['switcher'];
+
+  connect(e) {
+    this.switcherTarget.checked = ![...document.documentElement.classList].includes('dark');
+  }
+
   toggleTheme(e) {
     document.documentElement.classList.toggle('dark')
   }
