a941fd50947cf6fce24b04630832526d60df9504
loqimean <vanuha277@gmail.com>
2022-04-04 08:36:56 +0300

    feat(seeds.rb) add user seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Start creating admin user'
+User.create!(first_name: 'Ivan', last_name: 'Admin', password: 'simplesimple', email: 'admin@mail.io')
+puts 'Seeding is done!'
