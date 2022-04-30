feb5ef9a21801c0eb28a05c72840e7a99b16bed7
loqimean <vanuha277@gmail.com>
2022-04-30 18:24:05 +0300

    chore(seeds.rb) add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
 puts 'Start creating admin user'
 User.create!(first_name: 'Ivan', last_name: 'Admin', password: 'simplesimple', email: 'admin@mail.io')
+
+puts 'Start creating regions'
+regions = RegionsSeed.run
+
+puts 'Start creating people'
+regions.each do |region_name|
+  city = FactoryBot.create(:city, region: Region.find_by(name: region_name))
+  FactoryBot.create_list(:person, rand(0..10), current_city: city, childhood_city: city)
+end
+
 puts 'Seeding is done!'
