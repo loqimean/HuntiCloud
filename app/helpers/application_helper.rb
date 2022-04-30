c319ec181f6f766f1a6b685cab439b509cbe416a
loqimean <vanuha277@gmail.com>
2022-04-30 14:03:07 +0300

    feat(helpers|views) render svg by currency

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,34 @@
 module ApplicationHelper
-  def active_link(path)
-    'bg-blue-200 ml-[-20px] rounded-r-full text-blue-900 dark:bg-gray-700 dark:text-white pl-8' if current_page?(path)
+  def svg_for(controller_name)
+    case controller_name
+    when :gallery
+      path = '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>'
+    when :people
+      path = '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path>'
+    else
+      raise "Unknown case for controller: #{controller_name}"
+    end
+
+    svg_base(path)
+  end
+
+  def svg_for_currency(currency_code)
+    case currency_code
+    when :eur
+      path = '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.121 15.536c-1.171 1.952-3.07 1.952-4.242 0-1.172-1.953-1.172-5.119 0-7.072 1.171-1.952 3.07-1.952 4.242 0M8 10.5h4m-4 3h4m9-1.5a9 9 0 11-18 0 9 9 0 0118 0z"></path>'
+    when :usd
+      path = '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>'
+    else
+      raise "Unknown currency: #{currency_code}"
+    end
+
+    svg_base(path)
+  end
+
+  def svg_base(path)
+    svg = '<svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" ' \
+    'stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">' + path + '</svg>'
+
+    svg.html_safe
   end
 end
