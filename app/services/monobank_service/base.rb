9bfaa6c3954ef89f87ebe180a43c37e2e54bdfc2
loqimean <vanuha277@gmail.com>
2022-06-05 17:40:39 +0300

    fix(services/monobank) add offline values

--- app/services/monobank_service/base.rb
+++ app/services/monobank_service/base.rb
@@ -6,6 +6,7 @@ class MonobankService::Base < BaseService
   def initialize; end
 
   def currency_by(sell_currency_iso_code:, buy_currency_iso_code: 980)
+
     currencies.find do |hash|
       hash['currencyCodeA'] == sell_currency_iso_code &&
       hash['currencyCodeB'] == buy_currency_iso_code
@@ -13,8 +14,13 @@ class MonobankService::Base < BaseService
   end
 
   def currencies
-    Rails.cache.fetch(CURRENCY_API, expires_in: 1.minutes) do
-      JSON.parse(self.call(CURRENCY_API))
+    begin
+      Rails.cache.fetch(CURRENCY_API, expires_in: 1.minutes) do # resolve problem with no internet connection
+        JSON.parse(self.call(CURRENCY_API))
+      end
+    rescue => exception
+      [{"currencyCodeA"=>978, "currencyCodeB"=>980, "rateBuy"=>00, "rateSell"=>00},
+      {"currencyCodeA"=>840, "currencyCodeB"=>980, "rateBuy"=>00, "rateSell"=>00}]
     end
   end
 end
