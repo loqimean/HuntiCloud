17ee0a7e499241ee220c2d157bfe0370c1927f91
loqimean <vanuha277@gmail.com>
2022-06-11 20:58:20 +0300

    save uploads and some other folders also 5 releases

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,10 @@ set :stage, :production
 set :rails_env, :production
 set :branch, 'master'
 set :passenger_restart_with_touch, true
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
 
 server '192.168.0.106', user: 'xi-master', roles: %w[app db web]
 
