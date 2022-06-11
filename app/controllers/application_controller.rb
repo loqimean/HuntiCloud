708c8334ea2504a63741be4821baffc41e8cceb2
loqimean <vanuha277@gmail.com>
2022-06-11 13:18:42 +0300

    feat(controllers|js|views) toggle dark theme also by cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:public_page]
+  before_action :set_theme_by_default
 
   def public_page; end # add route for this
+
+  private
+
+  def set_theme_by_default
+    cookies[:dark_theme] ||= 'true'
+  end
 end
--- app/javascript/controllers/dark_theme_controller.js
+++ app/javascript/controllers/dark_theme_controller.js
@@ -1,15 +1,31 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import { getCookie, setCookie } from "custom/cookies";
 
 export default class extends Controller {
   static targets = ['sun', 'moon'];
 
   connect(e) {
-    // this.switcherTarget.checked = ![...document.documentElement.classList].includes('dark');
+    this.themeCookieName = 'dark_theme';
+    this.isDarkThemeByCookie = getCookie(this.themeCookieName);
+
+    if (this.isDarkThemeByCookie === undefined) {
+      setCookie(this.themeCookieName, this.isDarkTheme);
+    }
   }
 
   toggleTheme(e) {
-    document.documentElement.classList.toggle('dark')
-    this.sunTarget.classList.toggle('hidden')
-    this.moonTarget.classList.toggle('hidden')
+    document.documentElement.classList.toggle('dark');
+    this.sunTarget.classList.toggle('hidden');
+    this.moonTarget.classList.toggle('hidden');
+
+    setCookie(this.themeCookieName, this.isDarkTheme);
+  }
+
+  get currentThemeByCoockie() {
+    getCookie(this.themeCookieName);
+  }
+
+  get isDarkTheme() {
+    return [...document.documentElement.classList].includes('dark');
   }
 }
